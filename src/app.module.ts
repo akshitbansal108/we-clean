d309636816521757473cd2586aec5f994a662f08
akshitbansal-bst <akakshitbansal@gmail.com>
2020-10-18 11:21:18 +0530

    users module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ServicesModule } from './services/services.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, ServicesModule],
+  imports: [AuthModule, ServicesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
