704ed9de82f9fc4d03506af898b97adec5ecd901
PragsJ <pragya2321@gmail.com>
2020-10-27 13:17:51 +0530

    Routing at Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,72 +1,19 @@
 import React from 'react';
 import './App.scss';
-import { Login, Register } from './components/login/index';
+// import { BrowseRouter as Router} from 'react-router-dom';
+// import Route from 'react-router-dom/Route';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Registration } from './components/index';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoginActive: true,
-    };
-  }
-
-  componentDidMount() {
-    //Add .right by default
-    this.rightSide.classList.add('right');
-  }
-
-  changeState() {
-    const { isLoginActive } = this.state;
-
-    if (isLoginActive) {
-      this.rightSide.classList.remove('right');
-      this.rightSide.classList.add('left');
-    } else {
-      this.rightSide.classList.remove('left');
-      this.rightSide.classList.add('right');
-    }
-    this.setState(prevState => ({ isLoginActive: !prevState.isLoginActive }));
-  }
-
   render() {
-    const { isLoginActive } = this.state;
-    const current = isLoginActive ? 'Register' : 'Login';
-    const currentActive = isLoginActive ? 'login' : 'register';
     return (
-      <div className="App">
-        <div className="login">
-          <div className="container" ref={ref => (this.container = ref)}>
-            {isLoginActive && (
-              <Login containerRef={ref => (this.current = ref)} />
-            )}
-            {!isLoginActive && (
-              <Register containerRef={ref => (this.current = ref)} />
-            )}
-          </div>
-          <RightSide
-            current={current}
-            currentActive={currentActive}
-            containerRef={ref => (this.rightSide = ref)}
-            onClick={this.changeState.bind(this)}
-          />
-        </div>
-      </div>
+      <Router>
+        <Route exact path="/login/" component={Registration} />
+
+      </Router>      
     );
   }
-}
-
-const RightSide = props => {
-  return (
-    <div
-      className="right-side"
-      ref={props.containerRef}
-      onClick={props.onClick}
-    >
-      <div className="inner-container">
-        <div className="text">{props.current}</div>
-      </div>
-    </div>
-  );
 };
 
 export default App;
